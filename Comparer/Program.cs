76960dbc2143b8920753eaec51ea3abe64cecfbb
MartisiusL <31843212+MartisiusL@users.noreply.github.com>
2017-09-24 23:35:41 +0300

    Add Windows From
    Added windows form with controls and displays.

--- Comparer/Program.cs
+++ Comparer/Program.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.IO;
+using System.Windows.Forms;
 
 namespace Comparer
 {
-    static class Program
+    public static class Program
     {
+
+        
         // Solve dll problem
         static Program()
         {
@@ -21,22 +24,30 @@ namespace Comparer
         }
 
         [STAThread]
-        static void Main()
+        public static void Main()
+        {
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Main());
+        }
+
+        /*public static void MainProgram()
         {
             string inputFile = Navigator.SelectInputFile();
-            string outputFile = Navigator.SelectOutputFile();
-            string text = ImageRecognition.ExtractText(inputFile);
-            var textManager = new TextManager();
+           string outputFile = Navigator.SelectOutputFile();
+           string text = ImageRecognition.ExtractText(inputFile);
+           var textManager = new TextManager();
 
-            text = textManager.PrepareText(text);
-            ResultWriter(text, outputFile);
-        }
+           text = textManager.PrepareText(text);
+           ResultWriter(text, outputFile);
+        }*/
+
 
         public static void ResultWriter(string text, string filePath)
         {
             TextWriter textWriter = new StreamWriter(filePath);
             textWriter.Write(text);
-            //MessageBox.Show(text);
             textWriter.Close();
         }
     }

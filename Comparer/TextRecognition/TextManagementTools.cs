26f6ec5a4359583f4767183ad4bba503fc6cc020
osvaldasj <jonaitis.osvaldas@gmail.com>
2017-10-17 17:13:02 +0300

    Add Extension method TextManagementTools

---
+++ Comparer/TextRecognition/TextManagementTools.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Comparer.TextRecognition
+{
+    public static class TextManagementTools
+    {
+        // Add new line symbol at the end of string
+        public static string AddNewLine(this String str)
+        {
+            return str + "\n";
+        }
+
+        // Make all letters to lowercase
+        public static string Standartise(this String str)
+        {
+            return str.ToLower();
+        }
+    }
+}

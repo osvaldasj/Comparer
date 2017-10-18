41919206ad59331eeee45d75815a657f82e29fbc
RainingMammoths <vanagas.justas@gmail.com>
2017-10-18 19:42:43 +0300

    AdvancedTextManagementTools
    Create class that could show an example of Widening and Narrowing conversion

---
+++ Comparer/TextRecognition/AdvancedTextManagementTools.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Comparer.TextRecognition
+{
+    public static class AdvancedTextManagementTools : TextManagementTools
+    {
+        // Make all letters to uppercase
+        public static string Uppercase(this String str)
+        {
+            return str.ToUpper();
+        }
+
+        // Fix the string
+        public static string Fix(this String str, char wrong, char right)
+        {
+            return str.Replace(wrong, right); // example: Fix("pum", 'u', 'v'); will return "pvm"
+        }
+    }
+}

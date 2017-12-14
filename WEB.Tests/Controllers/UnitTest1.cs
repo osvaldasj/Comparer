55b3caa8feff799159c1864e22b9f6b727153f0b
LMartisius <31843212+MartisiusL@users.noreply.github.com>
2017-12-14 13:19:35 +0200

    AfterAS

---
+++ WEB.Tests/Controllers/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WEB;
+using WEB.Controllers;
+using System.Web.Mvc;
+
+namespace WEB.Tests.Controllers
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            LogInScreenController x = new LogInScreenController();
+            
+        }
+    }
+}
